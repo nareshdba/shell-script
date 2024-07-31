a67ff01296ef489aa5704673c612b32ab4d1db7d
naresh <naresh.dba.p@gmail.com>
2024-07-31 23:31:48 +0530

    changes done

--- 06-array.sh
+++ 06-array.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-MOVIES=("RRR" "DjTillu" "murari")
+MOVIES=("RRR" "DjTillu" "murari" "Salaar")
 
-# size of above array is 3.
-# index are 0,1,2
+# size of above array is 4.
+# index are 0,1,2,3
 
 # list always starts with 0.
 
 echo "First Movie is: ${MOVIES[0]}"
 echo "First Movie is: ${MOVIES[1]}"
 echo "First Movie is: ${MOVIES[@]}"
+echo "Best Movie is : ${MOVIES[3]}"
